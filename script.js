3a0a6555a28c50edd64a0d9382e49b2157a8b111
omurbek <omurbek.melisbekov.kk@gmail.com>
2024-03-13 22:36:12 +0600

    improved version

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-window.addEventListener("load", () => {
-const value = document.querySelector("input");
-const btn = document.querySelector(".btn");
-const list = document.querySelector(".list");
+const input = document.querySelector('[type="text"]');
+const ul = document.querySelector("ul");
+const button = document.querySelector("#button");
 
+button.addEventListener("click", (event) => {
+  if (input.value) {
+    event.preventDefault();
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    span.innerHTML = "+";
+    let firstLetter = input.value[0].toUpperCase();
+    li.innerHTML = firstLetter + input.value.slice(1);
 
-btn.addEventListener("click", function() {
-    let newList = document.createElement("li");
-    newList.className = "newlist"
-    list.appendChild(newList);
-    newList.innerHTML = value.value;
-    value.value = "";
+    ul.appendChild(li);
+    li.appendChild(span);
+    input.value = "";
+  } else {
+    event.preventDefault();
+  }
+});
 
-
-const lists = document.querySelectorAll("li");
-lists.forEach((x) => {
-    x.addEventListener("click", () => {
-    x.remove();
-})
-})
-
-})
-
-
-
-
-
-
+ul.addEventListener("click", (event) => {
+  if (event.target.tagName === "SPAN" && event.target.style.color !== "grey") {
+    event.target.closest("li").style.textDecoration = "line-through";
+    event.target.closest("li").style.color = "grey";
+    event.target.style.color = "grey";
+  } else if (event.target.style.color == "grey") {
+    event.target.closest("li").remove();
+  }
 });
